11c49c472227952548b13ed76fd8b5b784acf6e5
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-09-30 12:38:15 +0000

    2

--- collections.py
+++ collections.py
@@ -71,25 +71,100 @@
 # x = "#".join(colours)
 # print(x)
 
-# dictionaries
+# # dictionaries
 
-# drinks = {  "still": "water", 
-#             "fizzy": "cola", 
-#             "hot": "coffee"}
+# # drinks = {  "still": "water", 
+# #             "fizzy": "cola", 
+# #             "hot": "coffee"}
 
-# print(drinks["still"])
+# # print(drinks["still"])
 
-# print(drinks.items())
-# print(drinks.keys())
-# print(drinks.values())
+# # print(drinks.items())
+# # print(drinks.keys())
+# # print(drinks.values())
 
-# print(drinks.get("hot1"))
+# # print(drinks.get("hot1"))
 
 
-# Exercise:
-# Make a dictionary, with authors(2-3) + books(2-3 books)
-# input to ask for an author
-# print a string of the books
-# error handling for incorrect keys
-# try to only use things covered.  
+# # Exercise:
+# # Make a dictionary, with authors(2-3) + books(2-3 books)
+# # input to ask for an author
+# # print a string of the books
+# # error handling for incorrect keys
+# # try to only use things covered.  
 
+# # book_dictionary = {
+# # 	"N.T. Wright": ["The Bible for Everyone: A New Translation", "Surprised by Hope: Rethinking heaven, the resurrection and the mission of the Church"],
+# # 	"Martin Luther": ["95 Theses", "The Bondage of the Will", "Table Talk"],
+# # 	"Karl Barth": ["Church Dogmatics", "The Epistle to the Romans"]
+# # }
+
+# # author = input("Enter the name of the author: ")
+
+# # if author in book_dictionary:
+# # 	print("Books by " + author + ": ")
+# # 	books = ", ".join(book_dictionary[author])
+# # 	print(books)
+# # else:
+# # 	print("Cannot find any book by the author.")
+
+# booklist = {
+
+#     "J.K. Rowling": [
+#         "Harry Potter and the Sorcerer's Stone",
+#         "Harry Potter and the Chamber of Secrets",
+#         "Harry Potter and the Prisoner of Azkaban"
+#     ],
+#     "Roald Dahl": [
+#         "Charlie and the Chocolate Factory",
+#         "Matilda",
+#         "James and the Giant Peach"
+#     ],
+#     "Sheila McCullagh": [
+#         "The Land of the Blue Flower",
+#         "Tim and the Hidden People",
+#         "The Village with Three Corners"
+#     ]
+# }
+
+# print("Availible authors:")
+# print(", ".join(booklist.keys()))
+
+# author = input("please enter an authors name " )
+
+# books = booklist.get(author, []) #or ["sorry no books found"] 
+
+# print(", ".join(books) or "no books found")
+
+# JacksBookshop = {
+#   "author1": {
+#   "name": "Rick Riordan",
+#   "book1": "The Lightning Thief", 
+#   "book2": "The Sea of Monsters", 
+#   "book3": "The Last Olympian",
+# },
+#   "author2": {
+#   "name": "Jonathan Haidt",
+#   "book1": "The Happiness Hypothesis", 
+#   "book2": "The Righteous Mind",
+#   "book3": "The Anxious Generation",  
+# },
+#   "author3": {
+#   "name": "Colleen Hoover", 
+#   "book1": "It Ends With Us",
+#   "book2": "Verity",
+#   "book3": "Hopeless",    
+# }
+# }
+# y = JacksBookshop.get("author2").get("name")
+# print(y)
+
+# record  = JacksBookshop.get("author2")
+
+# list_to_print = []
+
+# for a, b in record.items():
+#     if "book" in a:
+#         list_to_print.append(b)
+
+# print(", ".join(list_to_print))
