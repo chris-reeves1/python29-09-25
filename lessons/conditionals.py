53e81192a96b12f32c8119b0071aafdf223ccf2a
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-09-30 13:36:04 +0000

    2

---
+++ lessons/conditionals.py
@@ -0,0 +1,50 @@
+# If, else, elif (else if)
+
+# eval()
+# compile()
+# exec()
+
+# on_holiday = True
+# is_admin = False
+# is_verifed = False
+                    
+                                     
+# if (is_admin or is_verifed) and on_holiday:
+#     print("access allowed")
+
+# import dis 
+
+# def example():
+#     x = 0
+#     while x < 3:
+#         print("something")
+#         x += 1
+
+# dis.dis(example)
+
+
+# temp = 15
+
+# if temp >= 30:
+#     print("Very hot")
+# elif temp >= 20:
+#     print("quite hot")
+# elif temp > 10:
+#     print("ok")
+# else:
+#     print("not good")
+
+
+# mulitiple comparators:
+
+deposit = 20
+password = "password"
+
+if 0 < deposit < 100 and password == "password":
+    print("deposited")
+else:
+    print("failed")
+
+
+
+
