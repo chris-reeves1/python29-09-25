fe5691d6a6c1fc1be773dfafb1176c2cb5171e8d
chris-reeves1 <dr_gonzo19@hotmail.com>
2025-09-30 13:36:09 +0000

    2

--- lessons/conditionals.py
+++ lessons/conditionals.py
@@ -37,14 +37,39 @@
 
 # mulitiple comparators:
 
-deposit = 20
-password = "password"
+# deposit = 20
+# password = "password"
 
-if 0 < deposit < 100 and password == "password":
-    print("deposited")
-else:
-    print("failed")
+# if 0 < deposit < 100 and password == "password":
+#     print("deposited")
+# else:
+#     print("failed")
+
+# if not 0 < deposit < 100 and password != "password":
+#     print("failed")
+# else:
+#     print("deposit")
 
 
+# user =  "root"
+
+# # if name in ("root", "admin", "user"):
+# #     print("invlaid")
+# # else:
+# #     print("accepted")
+
+# if name not in ("root", "admin", "user"):
+#     print("accepted")
+# else:
+#     print("invalid")
 
 
+Exercise:
+weight converter app
+- user to input weight
+- user to input a unit kgs or lbs
+- logic: check unit
+- logic: do a conversion
+- print out the result
+- stretch: error handling for unit type(upper/lower, else/while loop)
+- optional: error handling for non-numeric inputs for weight. 
